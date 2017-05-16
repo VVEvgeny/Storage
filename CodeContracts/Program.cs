18d2da658c2e9b1ec0502a8cafa7eab4b54822ad
Evgeny <vvevgeny@gmail.com>
2017-05-16 18:21:34 +0300

    +CodeContracts

---
+++ CodeContracts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace CodeContracts
+{
+    class Program
+    {
+        private static object o = null;
+        private static int TestMethod()
+        {
+            //ErrorInPreConditions
+            Contract.Requires(o != null);
+
+            //ErrorInPostConditions
+            Contract.Ensures(o is int);
+
+            //ErrorInReturn
+            Contract.Ensures(Contract.Result<int>() > 5);
+
+            return (int?) o ?? 0;
+        }
+        
+        static void Main(string[] args)
+        {
+            //o = 5;
+            TestMethod();
+        }
+    }
+}
